f4cd07966ea4d81cecdf9bb9577a21bee8860741
Anurag V <47092363+anurag-world@users.noreply.github.com>
2025-01-25 18:49:00 +0530

    Align bug fix

--- src/app/components/ScrollContainer.tsx
+++ src/app/components/ScrollContainer.tsx
@@ -26,26 +26,24 @@ export default function ScrollContainer({
   };
 
   return (
-    <div className="container mx-auto">
-      <div className={styles.container}>
-        <div className="container pl-4 max-sm:max-w-[490px]">
-          <h1 className="font-roboto text-2xl font-semibold tracking-wide text-blue-700 mb-6">
-            {title}
-          </h1>
+    <div className={styles.container}>
+      <div className="container pl-4 max-sm:max-w-[490px]">
+        <h1 className="font-roboto text-2xl font-semibold tracking-wide text-blue-700 mb-6">
+          {title}
+        </h1>
+      </div>
+
+      <div className="relative container max-sm:max-w-[490px]">
+        {/* Scrollable container */}
+        <div ref={scrollContainerRef} className={styles.companyCard}>
+          {content}
         </div>
 
-        <div className="relative container max-sm:max-w-[490px]">
-          {/* Scrollable container */}
-          <div ref={scrollContainerRef} className={styles.companyCard}>
-            {content}
-          </div>
+        {/* Left Arrow */}
+        <LeftScrollButtons onClick={() => scroll('left')} />
 
-          {/* Left Arrow */}
-          <LeftScrollButtons onClick={() => scroll('left')} />
-
-          {/* Right Arrow */}
-          <RightScrollButtons onClick={() => scroll('right')} />
-        </div>
+        {/* Right Arrow */}
+        <RightScrollButtons onClick={() => scroll('right')} />
       </div>
     </div>
   );
