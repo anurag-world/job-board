8307642a1bdea3e8251397d43c4117d38071b3c5
Anurag V <47092363+anurag-world@users.noreply.github.com>
2025-03-02 22:43:16 +0530

    add firebase file

---
+++ src/app/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBV08j4eW2xreRxtOQmD1LeI4i_zzzNJFY',
+  authDomain: 'kodebloc-jobs.firebaseapp.com',
+  projectId: 'kodebloc-jobs',
+  storageBucket: 'kodebloc-jobs.firebasestorage.app',
+  messagingSenderId: '986588775395',
+  appId: '1:986588775395:web:d24d301b5e4f0ce022d6d6',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
