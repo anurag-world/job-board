1ee74171bab16904068fd49b4b41dd2367fa71d2
Anurag V <47092363+anurag-world@users.noreply.github.com>
2025-03-03 17:10:08 +0530

    Add login & signup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,67 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { auth } from '@/lib/firebase/config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/context/AuthContext';
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/');
+    }
+  }, [user, router]);
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError('');
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      router.push('/'); // Redirect to HomePage
+    } catch (err) {
+      setError((err as Error).message);
+    }
+  };
+
   return (
-    <div className="flex h-screen items-center justify-center">
-      Login Page, Coming Soon!
+    <div className="flex flex-col h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold mb-4">Login</h1>
+      {error && <p className="text-red-500">{error}</p>}
+
+      <form className="flex flex-col space-y-4" onSubmit={handleLogin}>
+        <input
+          className="border p-2"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+          required
+        />
+        <input
+          className="border p-2"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+          required
+        />
+        <button className="bg-green-500 text-white py-2 px-4" type="submit">
+          Login
+        </button>
+      </form>
     </div>
   );
 }
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -17,7 +17,12 @@ interface AuthContextType {
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) throw new Error('useAuth must be used within an AuthProvider');
+  return context;
+};
 
 export default function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
